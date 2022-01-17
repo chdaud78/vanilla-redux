a407af77e6bd082e59e2b81eb690cf8c19a8daba
chdaud78 <qkrri56@naver.com>
2022-01-17 13:41:45 +0900

    [ENH] count number with redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
+import {createStore} from "redux";
 
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const number = document.querySelector(".number");
 
+const ADD = "ADD";
+const MINUS = "MINUS"
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-    number.innerText = count;
+const countModifier = (count = 0, action) => {
+    switch (action.type) {
+        case ADD :
+            return count + 1;
+        case MINUS :
+            return count - 1;
+        default:
+            return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
 }
 
-const handlePlus = () => {
-    count++;
-    updateText();
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({type: ADD});
 }
 
 const handleMinus = () => {
-    count--;
-    updateText();
+    countStore.dispatch({type: MINUS});
 }
 
-plus.addEventListener("click", handlePlus);
+plus.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
